816b4b05eb96be9da7723a52f596a68e9ad61ef8
Ilias Kaimakamis <61739781+iliaskam@users.noreply.github.com>
2022-06-30 22:07:40 +0300

    Delete TechChal directory

--- TechChal/src/com/company/RomanNumeralGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface RomanNumeralGenerator {
-    public void generate(int int_num); // convert from int -> roman
-    public void parse(String roman_num); // convert from roman -> int
-}
