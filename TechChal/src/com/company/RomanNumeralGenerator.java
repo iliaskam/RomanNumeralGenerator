fa83635a256c0c34efcfaed04953fb53894810be
Ilias Kaimakamis <61739781+iliaskam@users.noreply.github.com>
2022-06-30 21:53:36 +0300

    Add files via upload

---
+++ TechChal/src/com/company/RomanNumeralGenerator.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface RomanNumeralGenerator {
+    public void generate(int int_num); // convert from int -> roman
+    public void parse(String roman_num); // convert from roman -> int
+}
